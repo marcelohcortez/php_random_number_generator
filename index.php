e80d6e3dcfd2a676f0ca408441f2f633bd6420dc
Marcelo Cortez <marcelohcortez@gmail.com>
2022-05-19 16:18:01 +0100

    Create index.php file, add blank HTML code and add TailWind 'Play CDN' version

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Random Number Generator</title>
+        <script src="https://cdn.tailwindcss.com"></script>
+    </head>
+    <body>
+        <form>
+
+        </form>
+    </body>
+</html>
